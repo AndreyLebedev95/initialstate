aacd665d3568e7cc74831576665d233cfa984475
ao.lebedev <ao.lebedev@tensor.ru>
2018-11-30 13:18:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+app.get('/get_event_products', function(req, response){
+
+});
+app.post('/add_payer', function(req, res){
+
+});
+app.post('/remove_payer', function(req, res){
+
+});
+app.post('/remove_all_payers', function(req, res){
+
+});
+app.post('/get_debitor_list', function(req, res){
+
+});
+app.post('/upload', function(req, res) {
+
+});
+app.post('/add_payment', function(req, res) {
+
+});
+app.post('/change_payment_state', function(req, res) {
+
+});
+app.post('/get_payment', function(req, res) {
+
+});
+app.post('/delete_payment', function(req, res) {
+
+});
+app.post('/is_fixed', function(req, res) {
+
+});
+
+app.listen(3000, function(){
+	console.log('start');
+});
